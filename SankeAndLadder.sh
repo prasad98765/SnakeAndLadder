19a0a4091c78b4ae69c680fa8bfec5df1924460e
prasad98765 <pnchaudhari1996@gmail.com>
2019-12-12 16:59:01 +0530

    Added : To Check 'Noplay' 'Snake' 'Ladder' position Using Case

--- SankeAndLadder.sh
+++ SankeAndLadder.sh
@@ -1,11 +1,36 @@
-#!/bin/bash -x
-
-
+!/bin/bash -x
 echo "Welcome : To Sanke and Ladder Smulator Game"
 
 #variable 
 
 position=0
+noPlay=0
+ladder=1
+snake=2
 
 playerRoll=$((RANDOM%6+1))
-echo $playerRoll
+
+function nextMoves()
+{
+positionMoves=$((RANDOM%3))
+
+case $positionMoves in
+	$noPlay)
+				echo "No play"
+				;;
+	$ladder)
+				position=$(( $position + $playerRoll ))
+				;;
+	$snake)
+				position=$(( $position - $playerRoll ))
+				if [[ $position -lt 0 ]]
+				then
+						position=0
+				fi
+				;;
+esac
+}
+nextMoves
+echo $position			 
+
+
