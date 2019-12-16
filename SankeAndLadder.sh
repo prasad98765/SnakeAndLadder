b5b36c21d76b57032f6ab0c21d0dd17bc7e06d77
prasad98765 <pnchaudhari1996@gmail.com>
2019-12-16 15:11:34 +0530

    Added: Checking if the player has reached winning position

--- SankeAndLadder.sh
+++ SankeAndLadder.sh
@@ -1,5 +1,4 @@
-!/bin/bash -x
-echo "Welcome : To Sanke and Ladder Smulator Game"
+#!/bin/bash -x
 
 #variable 
 
@@ -7,30 +6,54 @@ position=0
 noPlay=0
 ladder=1
 snake=2
+count=0
+ 
+function checkWinOrNot()
+{
 
-playerRoll=$((RANDOM%6+1))
+   if [[ $position -eq 100 ]]
+   then
+      echo "Win"
+		echo "Total Die Roll Count" $count
+   else
+      nextMoves
+   fi
+
+}
+
 
 function nextMoves()
 {
-positionMoves=$((RANDOM%3))
+	playerRoll=$((RANDOM%6+1))
+	count=$(( $count + 1))
+	positionMoves=$((RANDOM%3))
 
-case $positionMoves in
-	$noPlay)
-				echo "No play"
-				;;
-	$ladder)
+	case $positionMoves in
+
+		$noPlay)
+			echo "No Play"
+			nextMoves
+			;;
+   	$ladder)
+			playerPosition=$(( $position + $playerRoll ))
+			if [[ $playerPosition -gt 100 ]]
+			then
+				nextMoves
+			else
 				position=$(( $position + $playerRoll ))
-				;;
-	$snake)
-				position=$(( $position - $playerRoll ))
-				if [[ $position -lt 0 ]]
-				then
-						position=0
-				fi
-				;;
-esac
+				echo $position
+				checkWinOrNot
+	   	fi;;
+   	$snake)
+			position=$(( $position - $playerRoll ))
+			if [[ $position -lt 0 ]]
+			then
+				position=0
+				nextMoves
+			else
+				checkWinOrNot
+			fi;;
+	esac
 }
+
 nextMoves
-echo $position			 
-
-
